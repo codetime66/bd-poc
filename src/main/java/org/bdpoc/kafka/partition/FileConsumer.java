f4cca2c86c5603aa3ddb090d93d16edc30fc8db9
codetime66 <carlosandrefernandes@gmail.com>
2016-11-21 23:59:36 -0200

    file consumer group tst

--- src/main/java/org/bdpoc/kafka/partition/FileConsumer.java
+++ src/main/java/org/bdpoc/kafka/partition/FileConsumer.java
@@ -9,11 +9,13 @@ import org.apache.kafka.common.errors.WakeupException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FileConsumer {
+
     private static Scanner in;
 
     public static void main(String[] argv) throws Exception {
@@ -38,6 +40,7 @@ public class FileConsumer {
     }
 
     private static class ConsumerThread extends Thread {
+
         private String topicName;
         private String groupId;
         private KafkaConsumer<String, String> kafkaConsumer;
@@ -47,7 +50,21 @@ public class FileConsumer {
             this.groupId = groupId;
         }
 
+        public int randInt() {
+            return randInt(1, 10000);
+        }
+        
+        public int randInt(int min, int max) {
+            Random rand = new Random();
+            int randomNum = rand.nextInt((max - min) + 1) + min;
+            return randomNum;
+        }
+
         public void run() {
+            
+            int consumer_id = randInt();
+            System.out.println("consumer ID: "+consumer_id);
+            
             Properties configProperties = new Properties();
             configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
             configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
@@ -60,29 +77,30 @@ public class FileConsumer {
                 public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
                     System.out.printf("%s topic-partitions are revoked from this consumer\n", Arrays.toString(partitions.toArray()));
                 }
+
                 public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
                     System.out.printf("%s topic-partitions are assigned to this consumer\n", Arrays.toString(partitions.toArray()));
                 }
             });
             //Start processing messages
             try {
-                long countIt=0l;
+                long countIt = 0l;
                 while (true) {
                     ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
-                    FileWriter out=null;
-                    boolean newFile=true;                    
-                    for (ConsumerRecord<String, String> record : records){
-                        if(newFile){
-                            countIt++;                            
-                            out = new FileWriter("./out/chunk_".concat(topicName.concat("_").concat(String.valueOf(countIt)).concat(".txt")));
-                            newFile=false;
+                    FileWriter out = null;
+                    boolean newFile = true;
+                    for (ConsumerRecord<String, String> record : records) {
+                        if (newFile) {
+                            countIt++;
+                            out = new FileWriter("./out/chunk_".concat(topicName.concat("_").concat(String.valueOf(consumer_id)).concat("_").concat(String.valueOf(countIt)).concat(".txt")));
+                            newFile = false;
                         }
                         out.write(record.value());
-                        out.write("\n");                        
+                        out.write("\n");
                     }
-                    if(out!=null){
-                       out.close();
-                    }                    
+                    if (out != null) {
+                        out.close();
+                    }
                 }
             } catch (WakeupException ex) {
                 System.out.println("Exception caught " + ex.getMessage());
@@ -99,5 +117,3 @@ public class FileConsumer {
         }
     }
 }
-
-
