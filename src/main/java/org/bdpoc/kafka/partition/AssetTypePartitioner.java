4f58452be7f757eeb80f747ac0241785bc99dc7f
codetime66 <carlosandrefernandes@gmail.com>
2016-11-21 22:07:14 -0200

    patition test

---
+++ src/main/java/org/bdpoc/kafka/partition/AssetTypePartitioner.java
@@ -0,0 +1,46 @@
+package org.bdpoc.kafka.partition;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AssetTypePartitioner implements Partitioner {
+    private static Map<String,Integer> assetTypeToPartitionMap;
+
+    //called at the start.
+    public void configure(Map<String, ?> configs) {
+        System.out.println("Inside CountryPartitioner.configure " + configs);
+        assetTypeToPartitionMap = new HashMap<String, Integer>();
+        for(Map.Entry<String,?> entry: configs.entrySet()){
+            if(entry.getKey().startsWith("partitions.")){
+                String keyName = entry.getKey();
+                String value = (String)entry.getValue();
+                System.out.println( keyName.substring(11));
+                int paritionId = Integer.parseInt(keyName.substring(11));
+                assetTypeToPartitionMap.put(value,paritionId);
+            }
+        }
+    }
+
+    //This method will get called once for each message
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes,
+                         Cluster cluster) {
+        List partitions = cluster.availablePartitionsForTopic(topic);
+        String valueStr = (String)value;
+        String countryName = ((String) value).split(":")[0];
+        if(assetTypeToPartitionMap.containsKey(countryName)){
+            //If the country is mapped to particular partition return it
+            return assetTypeToPartitionMap.get(countryName);
+        }else {
+            //If no country is mapped to particular partition distribute between remaining partitions
+            int noOfPartitions = cluster.topics().size();
+            return  value.hashCode()%noOfPartitions + assetTypeToPartitionMap.size() ;
+        }
+    }
+
+    //called at the end.
+    public void close() {}
+}
